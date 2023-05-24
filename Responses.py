17adf4367bf3a725378da3af46476da7fe177d92
Catguyrussell <132680739+Catguyrussell@users.noreply.github.com>
2023-05-24 08:16:52 -0500

    Update Responses.py
    Added topdeckmodern

--- Responses.py
+++ Responses.py
@@ -21,6 +21,9 @@ def handle_response(message) -> str:
     if p_message == 'hello':
         return 'Hey there!'
         
+    if p_message == '!topdeckpioneer':
+        return 'https://www.mtggoldfish.com/metagame/pioneer#paper'
+        
     if p_message == 'rolld4':
         return str(random.randint(1, 4))
     
