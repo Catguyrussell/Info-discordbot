9e61460b0b7665c4a3c1854c96de4d39e4ac6e02
Catguyrussell <132680739+Catguyrussell@users.noreply.github.com>
2023-05-23 20:24:06 -0500

    Update Responses.py
    added all dice and 5E tools link

--- Responses.py
+++ Responses.py
@@ -3,11 +3,41 @@ import random
 
 def handle_response(message) -> str:
     p_message = message.lower()
+    if p_message == '!5einfo':
+        return 'Hey there! Heres 5E tools https://5e.tools/'
+    
+    if p_message == 'hello':
+        return 'Hey there!'
+    
+    if p_message == 'hello':
+        return 'Hey there!'
+    
+    if p_message == 'hello':
+        return 'Hey there!'
+    
     if p_message == 'hello':
         return 'Hey there!'
 
-    if p_message == 'roll':
+    if p_message == 'rolld4':
+        return str(random.randint(1, 4))
+    
+    if p_message == 'rolld6':
         return str(random.randint(1, 6))
+    
+    if p_message == 'rolld8':
+        return str(random.randint(1, 8))
+    
+    if p_message == 'rolld10':
+        return str(random.randint(1, 10))
+    
+    if p_message == 'rolld12':
+        return str(random.randint(1, 12))
+    
+    if p_message == 'rolld20':
+        return str(random.randint(1, 20))
+    
+    if p_message == 'rolld100':
+        return str(random.randint(1, 100))
 
     if p_message == '!help':
         return "`This is a help message that you can modify.`"
